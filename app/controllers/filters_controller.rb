7aaf204df61999b8860f63d8987deada71402e90
nandisha <nandisha@softsuave.com>
2021-04-16 20:16:35 +0530

    add sort by

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,7 +2,7 @@ class FiltersController < ApplicationController
   require 'fetch_data'
 
   def fetch_track
-    tracks = FetchData.call(params).sort_by(&:album_name)
+    tracks = FetchData.call(params)&.sort_by(&:album_name)
 
     render json: tracks
   end
